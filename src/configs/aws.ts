814fa09f4e3e8d4f866920234b6260f3e3e3d057
Derek Wang <birdinforest@gmail.com>
2021-12-15 23:16:29 +1100

    feat: Config aws to access new user pool.

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,5 +1,5 @@
 export const COGNITO = {
-  REGION: 'arn:aws:cognito-idp:ap-southeast-2:825926907109:userpool/ap-southeast-2_xVfLDmr8b',
-  USER_POOL_ID: 'ap-southeast-2_xVfLDmr8b',
-  APP_CLIENT_ID: '10h1s9cvbke8g5uts9f5ehp5ke',
+  REGION: 'arn:aws:cognito-idp:ap-southeast-2:237624127646:userpool/ap-southeast-2_niFvoqjO4',
+  USER_POOL_ID: 'ap-southeast-2_niFvoqjO4',
+  APP_CLIENT_ID: 'drcsdp3fq8eb6o3rhuock93ue',
 };
